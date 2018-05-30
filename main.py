36b5992454822983462062d8862bff68573bfe7d
caos <helmut.enockl@gmail.com>
2018-05-30 20:58:15 +0200

    [feature/analysis] Añadido clase para detectar positivos y negativos según el parámetro deseado

--- main.py
+++ main.py
@@ -2,36 +2,45 @@ import pandas as pd
 
 
 
-flights = pd.read_table('infovuelos_sample.csv', sep=';')
-
-#Eliminamos vuelos duplicados
-flights=flights.drop_duplicates('flightNumber')
 
 
 
-flightsDict = dict()
 
-#parametrizar row['flightNumber' y convertir lo siguiente en una funcion,de esta forma podremos ir probando segun compañia,vuelo,aeropuerto o bien clima
+
+
 #podemos añadir tambien en el dict el tiempo de retraso.
-for index, row in flights.iterrows():
-    tempDict = dict()
-    if row['flightNumber'] in flightsDict:
-        tempDict= flightsDict.get(row['flightNumber'])
-        if row['dep_status'].find("Salida prevista"): # cuando pone esto entiendo que sale de forma puntual 5+- min (revisar)
-            tempDict['positivo']=tempDict.get['positivo']+1
+
+def analyze( flights,parameter):
+    flightsDict=dict()
+    for index, row in flights.iterrows():
+        tempDict = dict()
+        if row[parameter] in flightsDict:
+            tempDict= flightsDict.get(row[parameter])
+            if row['dep_status'].find("Salida prevista"): # cuando pone esto entiendo que sale de forma puntual 5+- min (revisar)
+                tempDict['positivo']=tempDict['positivo']+1
+            else:
+                tempDict['negativo'] = tempDict['negativo'] + 1
         else:
-            tempDict['negativo'] = tempDict.get['negativo'] + 1
-    else:
-        if row['dep_status'].find("Salida prevista"):  # cuando pone esto entiendo que sale de forma puntual 5+- min (revisar)
-            tempDict.setdefault('positivo',1)
-            tempDict.setdefault('negativo', 0)
+            if row['dep_status'].find("Salida prevista"):  # cuando pone esto entiendo que sale de forma puntual 5+- min (revisar)
+                tempDict.setdefault('positivo',1)
+                tempDict.setdefault('negativo', 0)
 
-        else:
-            tempDict.setdefault('positivo', 0)
-            tempDict.setdefault('negativo', 1)
+            else:
+                tempDict.setdefault('positivo', 0)
+                tempDict.setdefault('negativo', 1)
 
-    flightsDict[row['flightNumber']]=tempDict
-
+        flightsDict[row[parameter]]=tempDict
+    return flightsDict
 
 
-print("{" + "\n".join("{}: {}".format(k, v) for k, v in flightsDict.items()) + "}")
+if __name__ == "__main__":
+
+    flights = pd.read_table('infovuelos_sample.csv', sep=';')
+    # Eliminamos vuelos duplicados
+    flights = flights.drop_duplicates('flightNumber')
+
+    #flightsDict=analyze(flights,'flightNumber');
+    #print("{" + "\n".join("{}: {}".format(k, v) for k, v in flightsDict.items()) + "}")
+
+    flightsDict = analyze(flights, 'dep_airport_name');
+    print("{" + "\n".join("{}: {}".format(k, v) for k, v in flightsDict.items()) + "}")
