fdfa3a1c31a182f64625d2a54531a1b511fea08c
caos <helmut.enockl@gmail.com>
2018-06-04 23:12:13 +0200

    [feature/analysis] añadido funcion para generar graficos. v1

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from pylab import figure, title, xlabel, ylabel, xticks, bar, \
+                  legend, axis, savefig
+
 
 
 
@@ -32,8 +35,38 @@ def analyze( flights,parameter):
         flightsDict[row[parameter]]=tempDict
     return flightsDict
 
+#Si se pasan muchos valores se ve muy apretado. Revisar.
+def plot(keyList,positiveList,negativeList):
+    nucleotides = keyList
+
+    counts = [
+        positiveList,
+        negativeList,
+    ]
+
+    figure(num=None, figsize=(80, 60), dpi=400, facecolor='w', edgecolor='k')
+
+    title('Retraso de vuelos por dep_airport_name')
+    ylabel('%')
+
+
+
+    N = len(keyList)
+    x = range(N)
+    x2 = [x3 - 0.25 for x3 in x]
+    x4 = [x3 - 0.125 for x3 in x]
+    xticks(x4, nucleotides)
+    xticks(rotation=90)
+    bar(x, counts[0], width=0.2, color="#0c04e5", label="Sin Retraso %")
+
+    bar(x2,counts[1], width=0.2, color="#ef0202", label="Con Retraso %")
+
+    legend()
+
+    savefig('barplot.png')
 
 if __name__ == "__main__":
+
 
     flights = pd.read_table('infovuelos_sample.csv', sep=';')
     # Eliminamos vuelos duplicados
@@ -44,11 +77,18 @@ if __name__ == "__main__":
 
     flightsDict = analyze(flights, 'dep_airport_name');
     #print("{" + "\n".join("{}: {} ->".format(key, value,) for key, value in flightsDict.items()) + "}")
-
+    keyList=[]
+    positiveList=[]
+    negativeList=[]
     for key, value in flightsDict.items():
+
         positivo=value['positivo']
         negativo = value['negativo']
         positivoPercent = positivo / float(positivo + negativo)*100
         negativoPercent = negativo / float(positivo + negativo)*100
-        print (key +" ->Positivos: "+str(positivoPercent)+"% Negativos "+str(negativoPercent)+"%");
+        keyList.append(key)
+        positiveList.append(round(positivoPercent,2))
+        negativeList.append(round(negativoPercent,2))
 
+        print (key +" ->Positivos: "+str(positivoPercent)+"% Negativos "+str(negativoPercent)+"%");
+    plot(keyList,positiveList,negativeList)
