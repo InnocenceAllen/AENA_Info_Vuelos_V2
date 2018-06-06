e1f54d6549f79a58e27f3408f560c64bc38d1c16
Mario Gómez Martínez <magomar@gmail.com>
2018-06-06 20:54:15 +0200

    Primer versión de preprocesado
    Genera los retrasos, indica si aeropuerto es internacional, y guarda solo los datos relevantes

--- info_vuelos_analysis/constants.py
+++ info_vuelos_analysis/constants.py
@@ -1,4 +1,6 @@
 DATA_FIELDS = ['flightNumber', 'plane', 'dep_date', 'dep_time', 'dep_airport_code',
                'dep_weather_min', 'dep_weather_max', 'dep_weather_desc', 'arr_date', 'arr_time',
-               'arr_airport_code', 'arr_weather_min', 'arr_weather_max', 'arr_weather_desc', 'dep_delay', 'arr_delay']
+               'arr_airport_code', 'arr_weather_min', 'arr_weather_max', 'arr_weather_desc', 'dep_delay', 'arr_delay', 'dep_int', 'arr_int']
+FIELDS_DISCARDED = ['dep_airport_name', 'dep_terminal', 'dep_status', 'dep_counter', 'dep_door', 'arr_airport_name',
+                    'arr_terminal', 'arr_status','arr_room', 'arr_belt']
 CSV_DELIMITER = ';'
